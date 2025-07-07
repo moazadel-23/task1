d5fc3d2da93f8701a49ef1d8b62bbfdd4b6a758b
2024 <2024@DESKTOP-8BIE5S5>
2025-07-07 13:01:10 +0300

    Add project files.

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter smallCarpet count ");
+            int smallCarpet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter largeCarpet count ");
+            int largeCarpet = Convert.ToInt32(Console.ReadLine());
+
+            double tax = 0.06; // 6%
+            double cost = (smallCarpet * 3) + (largeCarpet * 1);
+            double totalTax = tax * cost;
+            double totalEstimate = cost + totalTax;
+
+
+            Console.WriteLine($"smallCarpet of price = {smallCarpet * 3} $");
+            Console.WriteLine($"largeCarpet of price = {largeCarpet * 1} $");
+            Console.WriteLine($"Cost = {cost} $");
+            Console.WriteLine($"totalTax = {tax * cost} $");
+            Console.WriteLine($"totalEstimate = {totalEstimate} $");
+            Console.WriteLine("this estimate is valid for 30 days");
+
+        }
+    }
+}
